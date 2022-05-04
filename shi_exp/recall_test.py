138710a3c18ae2b233a53b7525d697709928aba3
shi gengtian <shigengtian@gmail.com>
2022-05-04 16:05:38 +0900

    modify

--- shi_exp/recall_test.py
+++ shi_exp/recall_test.py
@@ -115,6 +115,7 @@ def get_color_rate_by_uid(df, df_items):
     result = result[[uid, color_col, 'color_rate']]
     return result
 
+
 def get_index_name_rate_by_uid(df, df_items):
     tmp = df.merge(df_items, on=[iid], how='left')
 
@@ -129,6 +130,7 @@ def get_index_name_rate_by_uid(df, df_items):
     result = result[[uid, index_name_col, 'index_rate']]
     return result
 
+
 def get_prodcut_name_rate_by_uid(df, df_items):
     tmp = df.merge(df_items, on=[iid], how='left')
 
@@ -138,9 +140,10 @@ def get_prodcut_name_rate_by_uid(df, df_items):
     tmp_total = tmp.groupby([uid])[time_col].agg('count').reset_index()
     tmp_total.columns = [uid, 'total_cnt']
     result = tmp_product_name.merge(tmp_total, on=uid, how='left')
-    result['prodcut_name_rate'] = result['cnt']/result['total_cnt']
+    result['prodcut_name_rate'] = result['cnt'] / result['total_cnt']
     result = result[[uid, product_group_name_col, 'prodcut_name_rate']]
     return result
+
 
 if __name__ == '__main__':
     test = True
@@ -149,25 +152,26 @@ if __name__ == '__main__':
         percentage = '1'
         transactions = pd.read_parquet('dataset/transactions_train_sample1.parquet')
         items = pd.read_parquet('dataset/articles_train_sample1.parquet')
-        users = pd.read_parquet('dataset/customers_sample01.parquet')
+        users = pd.read_parquet('dataset/customers_sample1.parquet')
+        df_val = pd.read_parquet('dataset/df_val1.parquet')
     else:
         transactions = pd.read_parquet('dataset/transactions_train.parquet')
         items = pd.read_parquet('dataset/articles.parquet')
         users = pd.read_parquet('dataset/customers.parquet')
 
-    if False:
+    if True:
         pop_items = get_pop_item(transactions, items)
         pop_items = reduce_mem(pop_items)
         pop_items.to_parquet('result/pop_items.parquet')
-    if False:
+        # if False:
         color_rate_by_uid = get_color_rate_by_uid(transactions, items)
         color_rate_by_uid = reduce_mem(color_rate_by_uid)
         color_rate_by_uid.to_parquet('result/color_rate_by_uid.parquet')
-    if False:
+        # if False:
         index_group_rate_by_uid = get_index_name_rate_by_uid(transactions, items)
         index_group_rate_by_uid = reduce_mem(index_group_rate_by_uid)
         index_group_rate_by_uid.to_parquet('result/index_group_rate_by_uid.parquet')
-    if False:
+        # if False:
         prodcut_name_rate_by_uid = get_prodcut_name_rate_by_uid(transactions, items)
         prodcut_name_rate_by_uid = reduce_mem(prodcut_name_rate_by_uid)
         prodcut_name_rate_by_uid.to_parquet('result/prodcut_name_rate_by_uid.parquet')
@@ -176,10 +180,34 @@ if __name__ == '__main__':
     color_rate_by_uid = pd.read_parquet('result/color_rate_by_uid.parquet')
     index_group_rate_by_uid = pd.read_parquet('result/index_group_rate_by_uid.parquet')
     prodcut_name_rate_by_uid = pd.read_parquet('result/prodcut_name_rate_by_uid.parquet')
-    print(color_rate_by_uid.dtypes)
 
     df_rate = color_rate_by_uid.merge(index_group_rate_by_uid, on=[uid], how='left')
-    
+    df_rate = df_rate.merge(prodcut_name_rate_by_uid, on=[uid], how='left')
+
+    # customer_ids = df_val[uid].unique()
+    # df_rate = df_rate[df_rate[uid].isin(customer_ids)]
+    customer_ids = df_rate[uid].unique()
+    print(len(customer_ids))
+    for customer_id in tqdm(customer_ids):
+        df_rate_ = df_rate[df_rate[uid] == customer_id]
+        df_rate_ = df_rate_.merge(pop_items, on=[color_col, index_name_col, product_group_name_col], how='left')
+        df_rate_ = df_rate_.dropna()
+
+    # for _customer_id in customer_ids:
+    #     print(_customer_id)
+    #     df_rate_by_customer = df_rate[df_rate[uid] == _customer_id]
+    #     print(df_rate_by_customer)
+    #     break
+
+    # for _, row in df_val.iterrows():
+    #     print(row[uid])
+    #     # rate = df_rate[df_rate[]]
+    #     break
+    # print(len(df_val[uid].unique()))
+
+    # for user_id in df_val[uid].unique():
+    #     print(user_id)
+
     # print(items[index_name_col].unique())
     # # age bin
     # bin_list = [-1, 19, 29, 39, 49, 59, 69, 119]
