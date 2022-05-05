585993af1f636a2a75809acd32acf9bf79304a4e
Sifat Ahmed <sifat20210601@gmail.com>
2022-05-05 15:17:13 +0900

    0.271

--- shi_exp/rank_lgb.py
+++ shi_exp/rank_lgb.py
@@ -9,7 +9,7 @@ import pathlib
 import cudf
 import numpy as np
 from xgboost import train
-from utils import Logger, reduce_mem
+from utils import Logger, reduce_mem, metrics
 import signal
 import multitasking
 import gc
@@ -45,117 +45,37 @@ def norm_sim(sim_df, weight=0.0):
     return sim_df
 
 
-def train_lgb(df_train_set, topk=12, offline=True):
-    ts = time.time()
-
-    """
-    :param df_train_set
-    :return:
-    """
-    fold = 5
-    ycol = 'label'
-    feature_names = list(
-        filter(lambda x: x not in [ycol, 'sales_channel_id', 'customer_id', 'article_id', 'week'],
-               df_train_set.columns))
-
-    df_importance_list = []
-    score_list = []
-    score_df = df_train_set[['customer_id', 'article_id', 'label']]
-
-    # kfold = GroupKFold(n_splits=fold)
-
-    # for fold_id, (trn_idx, val_idx) in enumerate(
-    #         kfold.split(df_train_set[feature_names], df_train_set[ycol], df_train_set['customer_id'])):
-
-    user_set = get_kfold_users(df_train_set, n=fold)
-
-    # for fold_id, (trn_idx, val_idx) in enumerate(
-    #         kfold.split(df_train_set[feature_names], df_train_set[ycol], df_train_set['customer_id'])):
-    for fold_id, valid_user in enumerate(user_set):
-        X_train = df_train_set[~df_train_set['customer_id'].isin(valid_user)]
-        Y_train = df_train_set[df_train_set['customer_id'].isin(valid_user)]
-
-        lgb_Classfication = lgb.LGBMClassifier(boosting_type='gbdt', num_leaves=31, reg_alpha=0.0, reg_lambda=1,
-                                               max_depth=-1, n_estimators=5000, subsample=0.7, colsample_bytree=0.7,
-                                               subsample_freq=1,
-                                               learning_rate=0.01, min_child_weight=50, random_state=42, n_jobs=-1)
-
-        lgb_Classfication.fit(X_train[feature_names], X_train[ycol], eval_set=[(
-            Y_train[feature_names], Y_train[ycol])], eval_metric=['auc', ], early_stopping_rounds=200, verbose=100)
-
-        joblib.dump(lgb_Classfication, f'model/lgb_ranker{fold_id}.pkl')
-
-        df_importance = pd.DataFrame({
-            'feature_name':
-                feature_names,
-            'importance':
-                lgb_Classfication.feature_importances_,
-        })
-
-        df_importance_list.append(df_importance)
-
-        Y_train['pred_score'] = lgb_Classfication.predict_proba(
-            Y_train[feature_names], num_iteration=lgb_Classfication.best_iteration_)[:, 1]
-
-        Y_train.sort_values(by=['customer_id', 'pred_score'])
-        Y_train['pred_rank'] = Y_train.groupby(
-            ['customer_id'])['pred_score'].rank(ascending=False, method='first')
-
-        score_list.append(
-            Y_train[['customer_id', 'article_id', 'pred_score', 'pred_rank']])
-
-        del X_train, Y_train, X_val, Y_val, g_train, g_eval
-        gc.collect()
-
-    del df_train_set
-    gc.collect()
-
-    log.debug('*' * 20)
-    df_importance = pd.concat(df_importance_list)
-    df_importance = df_importance.groupby([
-        'feature_name'
-    ])['importance'].agg('mean').sort_values(ascending=False).reset_index()
-    log.debug(f'importance: {df_importance}')
-    log.debug('*' * 20)
-    score_df_ = pd.concat(score_list, axis=0)
-    score_df = score_df.merge(score_df_, how='left', on=[
-        'customer_id', 'article_id'])
-
-    score_df[['customer_id', 'article_id', 'pred_score', 'pred_rank',
-              'label']].to_parquet('result/trn_lgb_classification_feats.parquet', index=False)
-
-
 def get_kfold_users(trn_df, n=5):
     user_ids = trn_df['customer_id'].unique()
     user_set = [user_ids[i::n] for i in range(n)]
     return user_set
 
 
-def train_lgb_rank(df_train_set):
+def train_lgb_rank(df_train):
     ts = time.time()
 
     """
-    :param df_train_set
+    :param df_train
     :return:
     """
-    fold = 2
+
+    fold = 5
     ycol = 'label'
+    df_train = df_train.rename(columns={"buy": "label"}, errors="raise")
     feature_names = list(
         filter(lambda x: x not in [ycol, 'sales_channel_id', 'customer_id', 'article_id', 'week'],
-               df_train_set.columns))
+               df_train.columns))
     print(feature_names)
     df_importance_list = []
     score_list = []
-    score_df = df_train_set[['customer_id', 'article_id', 'label']]
+    
+    score_df = df_train[['customer_id', 'article_id', 'label']]
 
-    # kfold = GroupKFold(n_splits=5)
-    user_set = get_kfold_users(df_train_set, n=fold)
+    user_set = get_kfold_users(df_train, n=fold)
 
-    # for fold_id, (trn_idx, val_idx) in enumerate(
-    #         kfold.split(df_train_set[feature_names], df_train_set[ycol], df_train_set['customer_id'])):
     for fold_id, valid_user in enumerate(user_set):
-        X_train = df_train_set[~df_train_set['customer_id'].isin(valid_user)]
-        Y_train = df_train_set[df_train_set['customer_id'].isin(valid_user)]
+        X_train = df_train[~df_train['customer_id'].isin(valid_user)]
+        Y_train = df_train[df_train['customer_id'].isin(valid_user)]
 
         g_train = X_train.groupby(['customer_id'], as_index=False).count()[
             ycol].values
@@ -167,9 +87,25 @@ def train_lgb_rank(df_train_set):
                                     subsample_freq=1,
                                     learning_rate=0.01, min_child_weight=50, random_state=42, n_jobs=-1)
 
-        lgb_ranker.fit(X_train[feature_names], X_train[ycol], group=g_train,
-                       eval_set=[(Y_train[feature_names], Y_train[ycol])], eval_group=[
-                g_eval], early_stopping_rounds=200, verbose=100, eval_at=[12], eval_metric=['ndcg', ])
+        # lgb_ranker = lgb.LGBMRanker(
+        #     objective="lambdarank",
+        #     metric="ndcg",
+        #     boosting_type="dart",
+        #     n_estimators=1,
+        #     importance_type='gain',
+        #     force_col_wise=True,
+        #     verbose=10
+        # )
+
+        lgb_ranker = lgb_ranker.fit(
+            X_train[feature_names],
+            X_train[ycol],
+            group=g_train,
+        )
+
+        # lgb_ranker.fit(X_train[feature_names], X_train[ycol], group=g_train,
+        #                eval_set=[(Y_train[feature_names], Y_train[ycol])], eval_group=[
+        #         g_eval], early_stopping_rounds=200, verbose=100, eval_at=[12], eval_metric=['ndcg', ])
 
         joblib.dump(lgb_ranker, f'model/lgb{fold_id}.pkl')
 
@@ -185,7 +121,7 @@ def train_lgb_rank(df_train_set):
         Y_train['pred_score'] = lgb_ranker.predict(
             Y_train[feature_names], num_iteration=lgb_ranker.best_iteration_)
         Y_train['pred_score'] = Y_train[['pred_score']
-        ].transform(lambda x: norm_sim(x))
+                                        ].transform(lambda x: norm_sim(x))
 
         Y_train.sort_values(by=['customer_id', 'pred_score'])
         Y_train['pred_rank'] = Y_train.groupby(
@@ -197,7 +133,7 @@ def train_lgb_rank(df_train_set):
         del X_train, Y_train, g_train, g_eval
         gc.collect()
 
-    del df_train_set
+    del df_train
     gc.collect()
 
     log.debug('*' * 20)
@@ -210,10 +146,179 @@ def train_lgb_rank(df_train_set):
     score_df_ = pd.concat(score_list, axis=0)
     score_df = score_df.merge(score_df_, how='left', on=[
         'customer_id', 'article_id'])
+
     score_df[['customer_id', 'article_id', 'pred_score', 'pred_rank',
               'label']].to_parquet('result/trn_lgb_ranker_feats.parquet', index=False)
 
 
+def train_lgb(df_train):
+
+    params = {
+        "objective": "binary",
+        "boosting": "gbdt",
+        "learning_rate": 0.01,
+        "metric": "binary_logloss",
+        "seed": 42,
+        # 'verbose':-1
+    }
+
+    ycol = 'label'
+    df_train = df_train.rename(columns={"buy": "label"}, errors="raise")
+    fi = pd.DataFrame()
+    feature_names = list(
+        filter(lambda x: x not in [ycol, 'sales_channel_id', 'customer_id', 'article_id', 'week'],
+               df_train.columns))
+    fold = 5
+
+    df_importance_list = []
+    score_list = []
+ 
+    score_df = df_train[['customer_id', 'article_id', 'label']]
+    user_set = get_kfold_users(df_train, n=fold)
+
+    for fold_id, valid_user in enumerate(user_set):
+        X_train = df_train[~df_train['customer_id'].isin(valid_user)]
+        Y_train = df_train[df_train['customer_id'].isin(valid_user)]
+
+        tr_data = lgb.Dataset(X_train[feature_names], label=X_train[ycol])
+        vl_data = lgb.Dataset(Y_train[feature_names], label=Y_train[ycol])
+
+        model = lgb.train(params, tr_data, valid_sets=[tr_data, vl_data],
+                          num_boost_round=20000, early_stopping_rounds=100, verbose_eval=1000)
+
+        joblib.dump(model, f'model/lgb_classification{fold_id}.pkl')
+
+        df_importance = pd.DataFrame({
+            'feature_name':
+                feature_names,
+            'importance':
+                model.feature_importance,
+        })
+
+        df_importance_list.append(df_importance)
+
+        Y_train['pred_score'] = model.predict(
+            Y_train[feature_names], num_iteration=model.best_iteration)
+
+        # Y_train['pred_score'] = Y_train[['pred_score']
+        #                                 ].transform(lambda x: norm_sim(x))
+
+        Y_train.sort_values(by=['customer_id', 'pred_score'])
+        Y_train['pred_rank'] = Y_train.groupby(
+            ['customer_id'])['pred_score'].rank(ascending=False, method='first')
+
+        score_list.append(
+            Y_train[['customer_id', 'article_id', 'pred_score', 'pred_rank']])
+
+        # del X_train, Y_train, g_train, g_eval
+        gc.collect()
+
+    del df_train
+    gc.collect()
+
+    # log.debug('*' * 20)
+    # df_importance = pd.concat(df_importance_list)
+    # df_importance = df_importance.groupby([
+    #     'feature_name'
+    # ])['importance'].agg('mean').sort_values(ascending=False).reset_index()
+    # log.debug(f'importance: {df_importance}')
+    # log.debug('*' * 20)
+    # score_df_ = pd.concat(score_list, axis=0)
+    # score_df = score_df.merge(score_df_, how='left', on=[
+    #     'customer_id', 'article_id'])
+    score_df_ = pd.concat(score_list, axis=0)
+    score_df = score_df.merge(score_df_, how='left', on=[
+        'customer_id', 'article_id'])
+
+    score_df[['customer_id', 'article_id', 'pred_score', 'pred_rank',
+              'label']].to_parquet('result/trn_lgb_classifer_feats.parquet', index=False)
+    # fi_tmp = pd.DataFrame()
+    # fi_tmp['iter'] = 1
+    # fi_tmp['feature'] = model.feature_name()
+    # fi_tmp['importance'] = model.feature_importance(importance_type='gain')
+    # fi = fi.append(fi_tmp)
+
+    # # # cv
+    # vl_pred = model.predict(vl_x, num_iteration=model.best_iteration)
+    # valid_pred = df_val[['customer_id', 'article_id']].copy()
+    # valid_pred['pred_score'] = vl_pred
+    # valid_pred[['customer_id', 'article_id', 'pred_score']].to_parquet('result/trn_lgb_feats.parquet', index=False)
+
+
+def calculate_cv(df_test, df_predict):
+    '''
+        calculate cv
+    '''
+    df_test = df_test.groupby('customer_id')[
+        'article_id'].apply(list).reset_index()
+    df_test.columns = ['customer_id', 'valid_true']
+    # df_predict = pd.read_parquet('result/trn_lgb_ranker_feats.parquet')
+    df_predict = df_predict.sort_values(
+        ['customer_id', 'pred_score'], ascending=False)
+    df_predict = df_predict.groupby('customer_id')[
+        'article_id'].apply(list).reset_index()
+    df_predict.columns = ['customer_id', 'valid_pred']
+
+    merge = df_test.merge(df_predict, how='inner')
+    metrics(merge, 12)
+
+# def _stacking():
+#     trn_lgb_ranker_feats = pd.read_csv('result/trn_lgb_ranker_feats.csv')
+#     trn_lgb_cls_feats = pd.read_csv('result/trn_lgb_cls_feats.csv')
+#     pass
+
+
+
+# def submmit(sample):
+
+#     with open("dataset/customer_ids.pickle", 'rb') as file:
+#         customer_map = pickle.load(file)
+
+#     with open("dataset/article_ids.pickle", 'rb') as file:
+#         article_map = pickle.load(file)
+
+#     all_target_id = sample['customer_id'].tolist()
+#     # first_week_sales_pred_tmp = first_week_sales_pred[first_week_sales_pred['1st_week_sales_dat'] >= '2020/09/23']
+
+#     # メモリのケアのためバッチで推論を回す
+#     batchs = [all_target_id[i:i+BATCH_SIZE] for i in range(0, len(all_target_id), BATCH_SIZE)]
+    
+#     preds = []
+    
+#     for target_id in batchs:
+#         recom = get_reccomend(target_id, transactions, Ns, first_week_sales_pred_tmp)
+#         ml_test = add_features(recom, transactions, articles, customers, first_week_sales_pred_tmp, text_svd_df)
+
+#         test_pred = np.zeros(len(ml_test))
+#         models = pathlib.Path(OUTPUT_DIR + f'{exp_name}').glob('model*.pickle')
+
+#         for m in models:
+#             with open(m, 'rb') as f:
+#                 model = pickle.load(f)
+#         test_pred += model.predict(ml_test[features], num_iteration=model.best_iteration) / N_SEED
+
+#         test = ml_test[['customer_id', 'article_id']].copy()
+#         test['prob'] = test_pred
+#         test = test.sort_values(['customer_id', 'prob'], ascending=False)
+#         test = test.groupby('customer_id').head(12)
+#         preds.append(test)
+    
+#     del recom, ml_test, test_pred
+#     gc.collect()
+
+#     test = pd.concat(preds)
+#     test['article_id'] = test['article_id'].map(article_ids)
+#     test['customer_id'] = test['customer_id'].map(customer_ids)
+
+#     test = test.groupby('customer_id')['article_id'].apply(list).reset_index()
+
+#     sub = sample['customer_id'].map(customer_ids).to_frame()
+#     sub = sub.merge(test, on=['customer_id'], how='left')
+#     sub = sub.rename(columns={'article_id':'prediction'})
+#     assert(sub['prediction'].apply(len).min()==12)
+#     sub['prediction'] = sub['prediction'].apply(lambda x: ' '.join(x))
+#     sub.to_csv(OUTPUT_DIR + f'{exp_name}/{exp_name}_sub.csv', index=False)
+    
 def submit(recall_df, topk=12, model_name='formmat'):
     INPUT_DIR = '/home/shi/workspace/h-and-m-personalized-fashion-recommendations/'
     articles = pd.read_csv(INPUT_DIR + 'articles.csv',
@@ -228,6 +333,9 @@ def submit(recall_df, topk=12, model_name='formmat'):
 
     customers['customer_ids_int'] = customers['customer_id'].map(customer_map)
     articles['article_ids_int'] = articles['article_id'].map(article_map)
+
+    recall_df['pred_score'] = recall_df[['pred_score']
+                                        ].transform(lambda x: norm_sim(x))
 
     recall_df = recall_df.sort_values(by=['customer_id', 'pred_score'])
     recall_df['rank'] = recall_df.groupby(
@@ -251,27 +359,38 @@ def submit(recall_df, topk=12, model_name='formmat'):
     save_name = model_name + '_' + datetime.datetime.today().strftime('%m-%d-%H-%M') + '.csv'
     submit.to_csv(save_name, index=False, header=True)
 
-
-def _stacking():
-    trn_lgb_ranker_feats = pd.read_csv('result/trn_lgb_ranker_feats.csv')
-    trn_lgb_cls_feats = pd.read_csv('result/trn_lgb_cls_feats.csv')
-    pass
-
-
-def calculate_cv():
-    pass
-
-
 if __name__ == '__main__':
-    df_train_set = pd.read_parquet('result/rank_train.parquet')
-    print(len(df_train_set.customer_id.unique()))
-    df_val = pd.read_parquet('dataset/df_val.parquet')
-    print(len(df_val.customer_id.unique()))  # 68984
-    df_train_set=df_train_set[df_train_set.customer_id.isin(df_val.customer_id.unique())]
-    # train_lgb_rank(df_train_set)
-    train_lgb(df_train_set)
+        
+    if True:
+        # infer
+        with open("dataset/customer_ids.pickle", 'rb') as file:
+            customer_map = pickle.load(file)
 
-    # train_lgb(df_train_set)
-    # df = pd.read_parquet('result/trn_lgb_ranker_feats.parquet')
-    # df = pd.read_parquet('result/trn_lgb_ranker_feats.parquet') 
-    # submit(df)
+        with open("dataset/article_ids.pickle", 'rb') as file:
+            article_map = pickle.load(file)
+            
+        
+        pred_score = []
+        for i in range(5):
+            df = pd.read_parquet(f'exp01/sub{i}.parquet')
+            pred_score.append(df['pred_score'].values)
+            
+
+        df['article_id'] = df['article_id'].astype(np.int32)
+        df['pred_score'] = np.mean(np.stack(pred_score), axis=0)
+
+        df = df.sort_values(by=['customer_id', 'pred_score'], ascending=False)
+        df['rank'] = df.groupby(
+            ['customer_id'])['pred_score'].rank(ascending=False, method='first')
+
+        df = df[df['rank'] <= 12]
+        print(df.head())
+        df['article_id'] = df['article_id'].map(article_map)
+        df['customer_id'] = df['customer_id'].map(customer_map)
+
+        df = df.groupby('customer_id')[
+            'article_id'].apply(list).reset_index()
+        df = df.rename(columns={'article_id': 'prediction'})
+        df['prediction'] = df['prediction'].apply(lambda x: ' '.join(x))
+        print(df.head())
+        df.to_csv('result.csv', index=False)
